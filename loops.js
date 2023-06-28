90f571012d7835cd5121915595726f28db4ab52d
jeff-ohene <jeff.ohene10@gmail.com>
2023-06-28 19:06:58 +0000

    create loops.js

---
+++ loops.js
@@ -0,0 +1,12 @@
+// Print numbers from 1 to 20 using a for loop.
+for (let i = 1; i <= 20; i++) {
+    console.log(i);
+}
+
+// Print numbers from 1 to 50 that are multiples of 5
+
+for (let i = 1; i <= 50; i++) {
+    if(i % 5 === 0) {
+        console.log(i);
+    }
+}
