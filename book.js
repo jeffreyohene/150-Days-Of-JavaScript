4dc742c9f17a90b5a50e738a871f9d640f0990f5
jeffreyohene <jeff.ohene10@gmail.com>
2023-08-29 04:15:23 +0000

    update book.js

--- book.js
+++ book.js
@@ -1,10 +1,13 @@
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read;
-
-    this.info = function() {
-      return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`;
-    };
+	this.title = title
+	this.author = author
+	this.pages = pages
+	this.read = read
+	
+	this.info = function() {
+		return `"${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}.`
+	}
 }
+
+const tGG= new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
+console.log(tGG.info()); // "The Great Gatsby by F. Scott Fitzgerald, 180 pages, read"
